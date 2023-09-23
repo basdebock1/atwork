8dc03b12f0ae1c7fdf3f921ded3108860264ac5a
Bas De Bock <bas.debock@student.kdg.be>
2023-09-23 14:05:09 +0200

    If statement for news

--- resources/views/nieuws/index.blade.php
+++ resources/views/nieuws/index.blade.php
@@ -16,35 +16,20 @@
                         Volg ons zeker op sociale media. Daar verschijnen leuke posts, maar er komen ook informatieve
                         berichten op onze pagina's. Like dus zeker onze Facebook-pagina en volg ons op Instagram.
                     </p>
-                    <a class="link-secondary light" target="_blank" href="https://www.instagram.com/scouts.sintmartinus/"><i class="fab fa-instagram"></i> Instagram</a>
-                    <a class="link-secondary light" target="_blank" href="https://www.facebook.com/scoutsmartinus"><i class="fab fa-facebook"></i> Facebook</a>
+                    <a class="link-secondary light" target="_blank" href="https://www.instagram.com/scouts.sintmartinus/"><i
+                            class="fab fa-instagram"></i> Instagram</a>
+                    <a class="link-secondary light" target="_blank" href="https://www.facebook.com/scoutsmartinus"><i
+                            class="fab fa-facebook"></i> Facebook</a>
                 </div>
             </div>
             <div class="article">
-                <h2>Laatste nieuwtje</h2>
-                <x-article :article="$latestArticle" />
+                @if ($latestArticle)
+                    <h2>Laatste nieuwtje</h2>
+                    <x-article :article="$latestArticle" />
+                @else
+                    <h2>Geen berichten gevonden</h2>
+                @endif
             </div>
-            {{-- <div class="intro__info">
-                <div class="info__content">
-                    <div class="info__text">
-                        <h3>De Fluiter</h3>
-                        <p>
-                            Alle info over normale vergaderingen vind je in ons maandelijks boekje 'De Fluiter'. Je vind dit
-                            boekje tussen alle nieuwsartikels. Wanneer je doorklikt naar de detailpagina kan je daar het
-                            document openen.
-                        </p>
-                    </div>
-                    <div class="info__text">
-                        <h3>Sociale Media</h3>
-                        <p>
-                            Volg ons zeker op sociale media. Daar verschijnen leuke posts, maar er komen ook informatieve
-                            berichten op onze pagina's. Like dus zeker onze Facebook-pagina en volg ons op Instagram.
-                        </p>
-                        <a class="link-secondary" target="_blank" href="https://www.instagram.com/scouts.sintmartinus/"><i class="fab fa-instagram"></i> Instagram</a>
-                        <a class="link-secondary" target="_blank" href="https://www.facebook.com/scoutsmartinus"><i class="fab fa-facebook"></i> Facebook</a>
-                    </div>
-                </div>
-            </div> --}}
         </div>
     </section>
     <section>

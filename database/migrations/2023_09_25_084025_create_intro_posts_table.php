4336adf18dcc989ef033ad2c24aed619be70cfe5
Bas De Bock <bas.debock@student.kdg.be>
2023-09-25 13:46:09 +0200

    Migration

--- database/migrations/2023_09_25_084025_create_intro_posts_table.php
+++ database/migrations/2023_09_25_084025_create_intro_posts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('heroposts', function (Blueprint $table) {
+        Schema::create('intro_posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('tilte');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('heroposts');
+        Schema::dropIfExists('intro_posts');
     }
 };

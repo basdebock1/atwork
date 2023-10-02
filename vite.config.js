9bc606f184b3ec560bfb2d468d72d190b7ff144c
Bas De Bock <bas.debock@student.kdg.be>
2023-10-02 20:46:19 +0200

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/css/app.css",
+                "resources/js/app.js",
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+    },
 });

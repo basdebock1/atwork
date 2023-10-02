f8908a74a0bf28695dfa2ba8cf2e66f5743e1e82
Bas De Bock <bas.debock@student.kdg.be>
2023-10-02 20:47:28 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
-    },
 });
